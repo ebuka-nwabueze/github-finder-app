3721fd85cf18df674ab642bb076e65ec917ab38e
Ebuka Nwabueze <ebukanwabueze@gmail.com>
2022-03-11 13:23:32 +0100

    alert component added

--- src/components/context/alert/AlertContext.js
+++ src/components/context/alert/AlertContext.js
@@ -11,6 +11,10 @@ export const AlertProvider = ({children}) => {
 
     const setAlert = (msg,type) => {
         dispatch({type: "SET_ALERT", payload: { msg, type }})
+
+        setTimeout(() => {
+            dispatch({type: "REMOVE_ALERT"})
+        }, 3000);
     }
 
     return <AlertContext.Provider value={{setAlert, alert: state}}>
